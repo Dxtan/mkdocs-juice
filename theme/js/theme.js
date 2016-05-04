edb93afb52587a2a641aa3bacfdd02925c7aedeb
tandexiang@hoge.cn <tandexiang@hoge.cn>
2016-05-04 19:30:44 +0800

    new UI

--- theme/js/theme.js
+++ theme/js/theme.js
@@ -2,3 +2,13 @@
  * This JavaScript doesn't do anything. The file exists just to demonstrate
  * including static assets from the HTML in themes.
  */
+$(function(){
+    $('body').scrollspy({ target: '#navbar-example' });
+
+    $('.bs-docs-sidenav a').click(function (e) {
+        // e.preventDefault()
+        $(this).tab('show')
+    });
+
+})
+
